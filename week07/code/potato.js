11b26f56fb6055ee093e1715a2bb9e46b7d944cd
rebleo <rml444@nyu.edu>
2022-03-10 11:52:05 -0500

    week07

---
+++ week07/code/potato.js
@@ -0,0 +1,11 @@
+console.log("javascript is here!");
+
+function setup(){
+    createCanvas(500,500)
+    background(255);
+}
+
+function draw(){
+    fill(255, 0,0);
+    ellipse(mouseX,mouseY, 60);
+}
